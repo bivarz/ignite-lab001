cead3ed3eeb96aff25edba9fa082abeae27c3342
bivarz <bold058@einstein.br>
2022-06-26 23:08:35 -0300

    feat - 0005 | Creating Video Section

--- ignite-project001/src/components/LessonsCard.tsx
+++ ignite-project001/src/components/LessonsCard.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import {CheckCircle} from 'phosphor-react'
+import {CheckCircle , Lock} from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+interface LessonsProps {
+  title:string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
 
 
-const LessonsCard: React.FC = () => {
+const LessonsCard = (props: LessonsProps) => {
+
+  const isLessonAvailable = isPast(props?.availableAt)
+  const  availableDateFormatted = format(props.availableAt,"EEEE' • 'd' de 'MMMM' • 'k'h'mm", {locale: ptBR})
+
   return (
    <a href="#">
     <span className="text-gray-300">
-      Domingo • 20 de junho • 19h00
+      {availableDateFormatted}
     </span>
     <div className="rounded border border-grahy-500 p-4 mt-2">
       <header className="flex items-center justify-between">
-        <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
-          <CheckCircle size={20}/>
-          Conteúdo Liberado
+        {isLessonAvailable ? (
+           <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+           <CheckCircle size={20}/>
+           Conteúdo Liberado
+         </span>
+        ):(
+          <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+          <Lock size={20}/>
+          Em breve
         </span>
-        <span className="text-xs rounded px-2 py-[2p] text-white border border-green-300 font-bold">
-          Ao Vivo
+        )}       
+        <span className="text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold">
+          {props?.type === 'live' ? "Ao Vivo" : "Aula Prática"}
         </span>
       </header>
       <strong className='text-gray-200 mt-5 block'>
-        Abertura do evento Ignite labs
+       {props?.title}
       </strong>
     </div>
    </a>
--- ignite-project001/src/components/Sidebar.tsx
+++ ignite-project001/src/components/Sidebar.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
-
+import { gql, useQuery } from '@apollo/client'
 import LessonsCard from './LessonsCard'
 
+const GET_LESSONS_QUERY = gql`
+  query  {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    lessonType
+    slug
+    title
+    availableAt
+  }
+}
+`
+interface GetLessonsQueryResponse {
+  lessons:{
+    id: string;
+    title:string;
+    slug: string;
+    availableAt: Date
+    LessonType: 'live' | 'class'
+  }[]
+}
+
 const Sidebar: React.FC = () => {
+ 
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+ 
   return(
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma das aulas
       </span>
       <div className="flex flex-col gap-8">
-        <LessonsCard />
-        <LessonsCard />
-        <LessonsCard />
-        <LessonsCard />
-        <LessonsCard />
-        <LessonsCard />
-        <LessonsCard />
+        {data?.lessons.map(lesson =>{
+          return(
+            <LessonsCard 
+            title={lesson.title}
+            slug={lesson.slug}
+            availableAt={new Date(lesson.availableAt)}
+            type={lesson.LessonType}
+            key={lesson.id}
+            />
+          )
+        })}              
       </div>
     </aside>
   );
